b14e318ef6fb2ef8f3e750e21eb325663a26c388
Ferozkan05 <151832688+Ferozkan05@users.noreply.github.com>
2025-04-26 00:19:48 +0530

    Create variables.tf

---
+++ patient/variables.tf
@@ -0,0 +1,46 @@
+variable "task_name" {
+  description = "Task name"
+  type        = string
+}
+variable "service_name" {
+  description = "Service name"
+  type        = string
+}
+variable "cluster_name" {
+  description = "Cluster name"
+  type        = string
+}
+variable "image" {
+  description = "ECR Image name"
+  type        = string
+}
+variable "cport" {
+  description = "Containerport"
+  type        = number
+}
+variable "hport" {
+  description = "host port"
+  type        = number
+}
+
+variable "subnets" {
+  description = "Public subnet CIDRs"
+  type        = list(string)
+}
+
+variable "sg" {
+  description = "SG name"
+  type        = list(string)
+}
+variable "tg" {
+  description = "TG name"
+  type        = string
+}
+variable "lg" {
+  description = "LG name"
+  type        = string
+}
+variable "bucket" {
+  description = "bucket key name"
+  type        = string
+}
