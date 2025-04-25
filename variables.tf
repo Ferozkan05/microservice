397558190aa1c2d8c0c19199da6c4bd9cb956632
Ferozkan05 <151832688+Ferozkan05@users.noreply.github.com>
2025-04-25 19:41:23 +0530

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,35 @@
+variable "task_name" {
+  description = "Task name"
+  type        = string
+}
+variable "service_name" {
+  description = "Service name"
+  type        = string
+}
+variable "cluster_name" {
+  description = "Cluster name"
+  type        = string
+}
+variable "image" {
+  description = "ECR Image name"
+  type        = string
+}
+
+
+variable "subnets" {
+  description = "Public subnet CIDRs"
+  type        = list(string)
+}
+
+variable "sg" {
+  description = "SG name"
+  type        = string
+}
+variable "tg" {
+  description = "TG name"
+  type        = string
+}
+variable "elb" {
+  description = "ELB name"
+  type        = string
+}
