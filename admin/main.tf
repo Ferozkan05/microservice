85437917632a53db6eac74ed3649dc3829b6879f
Ferozkan05 <151832688+Ferozkan05@users.noreply.github.com>
2025-04-26 00:36:47 +0530

    Update main.tf

--- admin/main.tf
+++ admin/main.tf
@@ -12,6 +12,15 @@ provider "aws" {
 }
 
 module "ecs" {
-  source = "../modules/ecs"
-
+  source = "../modules/ecs-fz"
+task_name = "admin"
+service_name = "admi-svc"
+cluster_name = "arn:aws:ecs:ap-south-1:376120733871:cluster/dev-ecs-cluster"
+image = "376120733871.dkr.ecr.ap-south-1.amazonaws.com/dev-admin-repo:latest"
+subnets = [  "subnet-0385badd767f03c1e",  "subnet-0e2e89e5b49e20703"]
+sg = [ "sg-0f0c7482f9f1e92e2" ]
+tg = "arn:aws:elasticloadbalancing:ap-south-1:376120733871:targetgroup/dev-ecs-tg-admission/b5b95b8c99a272f7"
+cport = 3001
+hport = 3001
+lg = "ecs/admin"
 }
